996338b99010c1c00f1060dccb8241cc12375c35
saphirq22 <104495770+saphirq22@users.noreply.github.com>
2023-01-25 10:00:29 +0100

    prise en compte de + delements

--- 2048.py
+++ 2048.py
@@ -10,7 +10,7 @@ from matplotlib.figure import Figure
 
 nbLigne = 4
 nbCol = 4
-maMatrice = init_grid()
+#maMatrice = init_grid()
 
 interface = Tk()
 interface.geometry ("600x400")
@@ -22,22 +22,26 @@ frm2048.pack(side=RIGHT, expand=True, padx=10,pady=10)
 #frm20482 = Frame(frm2048, width = 300, height=150)
 #frm20482.pack(fill="both", expand=True, padx=10,pady=10)
 
-frmBtn = Frame(interface, background='#D9D9D9', width = 300, height=400)
+frmBtn = Frame(interface, background='#D9D9D9', width = 300, height=600)
 frmBtn.pack(side=LEFT, expand=True, padx=10,pady=10)
 frmBtn1 = Frame(frmBtn, background='#D9D9D9', width = 400)
 frmBtn1.pack(padx=5,pady=5, side=TOP)
 frmBtn2 = Frame(frmBtn, width = 300)
 frmBtn2.pack(padx=5,pady=5, side=BOTTOM)
+frmBtn3 = Frame(frmBtn, width = 300)
+frmBtn3.pack(padx=5,pady=5, side=BOTTOM)
 
 lblScore = Label(frmBtn1, width = 10, height=1, text='Score').grid(row=0,column=0)
 lblScore1 = Label(frmBtn1, width = 20, height=1, textvariable='0000').grid(row=0,column=1, columnspan=2)
+lblBest = Label(frmBtn1, width = 10, height=1, text='BEST').grid(row=1,column=0)
+lblBest1 = Label(frmBtn1, width = 20, height=1, textvariable='0000').grid(row=1,column=1, columnspan=2)
 
-lblScore1 = Label(frmBtn1,bg='#D9D9D9', width = 5, height=0, textvariable='0000').grid(row=1,column=2)
+lblScore1 = Label(frmBtn1,bg='#D9D9D9', width = 5, height=0, textvariable='0000').grid(row=2,column=2)
 
-buttonHaut = Button(frmBtn1, text="Haut", command="rollUp", width = 10).grid(row=2, column=1)
-buttonGauche = Button(frmBtn1, text="Gauche", command="rollLeft", width = 10).grid(row=3,column=0)
-buttonDroite = Button(frmBtn1, text="Droite", command="rollRight", width = 10).grid(row=3,column=2)
-buttonBas = Button(frmBtn1, text="Bas", command="rollDown", width = 10).grid(row=4, column=1)
+buttonHaut = Button(frmBtn1, text="Haut", command="rollUp", width = 10).grid(row=3, column=1)
+buttonGauche = Button(frmBtn1, text="Gauche", command="rollLeft", width = 10).grid(row=4,column=0)
+buttonDroite = Button(frmBtn1, text="Droite", command="rollRight", width = 10).grid(row=4,column=2)
+buttonBas = Button(frmBtn1, text="Bas", command="rollDown", width = 10).grid(row=5, column=1)
 
 #definition du graphe
 fig = Figure(figsize=(5, 4), dpi=25)
@@ -52,6 +56,10 @@ canvas = FigureCanvasTkAgg(fig, master=frmBtn2)  # A tk.DrawingArea.
 canvas.draw()
 canvas1 = FigureCanvasTkAgg(fig, master=frmBtn2)  # A tk.DrawingArea.
 canvas1.draw()
+canvas2 = FigureCanvasTkAgg(fig, master=frmBtn2)  # A tk.DrawingArea.
+canvas2.draw()
+canvas3 = FigureCanvasTkAgg(fig, master=frmBtn2)  # A tk.DrawingArea.
+canvas3.draw()
 
 #buttonHaut.pack(ipadx=20,ipady=10, pady=10, padx=10, side=TOP)
 #buttonGauche.pack(ipadx=20,ipady=10, padx=10, pady=10, side=LEFT)
@@ -65,11 +73,14 @@ canvas1.draw()
 
 for ligne in range(nbLigne):
     for colonne in range(nbCol):
-        valeur = maMatrice[ligne,colonne]
-        Label(frm2048, text='%s' % valeur, borderwidth=15, background="green").grid(row=ligne, column=colonne)
+        #valeur = maMatrice[ligne,colonne]
+        #Label(frm2048, text='%s' % valeur, borderwidth=15, background="green").grid(row=ligne, column=colonne)
+        Label(frm2048, text='%s' % 1, borderwidth=15, background="green").grid(row=ligne, column=colonne)
 
 canvas.get_tk_widget().grid(row=0,column=0)
 canvas1.get_tk_widget().grid(row=0,column=1)
+canvas2.get_tk_widget().grid(row=1,column=0)
+canvas3.get_tk_widget().grid(row=1,column=1)
 
 interface.mainloop()
 #interface.bind("<Up>", rollUp) # Flèche haut
